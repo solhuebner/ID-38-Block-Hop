94788e2cb981a8fd029e313cf7c83adb954307d9
Joeri Gantois <info@JO3RI.be>
2016-02-26 17:18:32 +0100

    first start

--- BLOCK_DEV/BLOCK_DEV.ino
+++ BLOCK_DEV/BLOCK_DEV.ino
@@ -10,3 +10,51 @@
  Game License: MIT : https://opensource.org/licenses/MIT
 
  */
+
+//determine the game
+#define GAME_ID 38
+
+#include <Arduboy.h>
+#include <ArduboyExtra.h>
+#include <sprites.h>
+#include <simple_buttons.h>
+#include "menu.h"
+#include "game.h"
+
+Arduboy arduboy;
+
+byte gameState;
+
+typedef void (*FunctionPointer) ();
+FunctionPointer mainGameLoop[] =
+{
+  stateMenuIntro,
+  stateMenuMain,
+  stateMenuHelp,
+  stateMenuPlay,
+  stateMenuInfo,
+  stateMenuSoundfx,
+  stateGamePlaying,
+  stateGamePause,
+  stateGameOver,
+};
+
+void setup()
+{
+  arduboy.start();
+  arduboy.setFrameRate(60);
+  gameState = 0;
+  if (EEPROM.read(EEPROM_AUDIO_ON_OFF)) soundYesNo = true;
+  menuSelection = 3;
+}
+
+void loop() {
+  if (!(arduboy.nextFrame())) return;
+  buttons.poll();
+  if (soundYesNo == true) arduboy.audio.on();
+  else arduboy.audio.off();
+  arduboy.clearDisplay();
+  mainGameLoop[gameState]();
+  arduboy.display();
+}
+
