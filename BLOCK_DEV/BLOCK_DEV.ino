9c2018f1e4f0682092338122c7f78ecfd30989d6
Joeri Gantois <info@JO3RI.be>
2016-02-26 16:34:04 +0100

    Create BLOCK_DEV.ino

---
+++ BLOCK_DEV/BLOCK_DEV.ino
@@ -0,0 +1,12 @@
+/*
+ Block Hop: http://www.team-arg.org/block-manual.html
+
+ Arduboy version 0.1:  http://www.team-arg.org/block-downloads.html
+
+ MADE by TEAM a.r.g. : http://www.team-arg.org/more-about.html
+
+ 2016 - 
+
+ Game License: MIT : https://opensource.org/licenses/MIT
+
+ */
